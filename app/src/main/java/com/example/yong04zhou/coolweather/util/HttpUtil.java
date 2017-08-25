132d0f670eae148f4b9d8250dc3b8e7230316573
yong04.zhou <yong04.zhou@phicomm.com>
2017-08-25 15:01:41 +0800

    OverView：TEST
    //add bug id here
    
    Description：

--- app/src/main/java/com/example/yong04zhou/coolweather/util/HttpUtil.java
+++ app/src/main/java/com/example/yong04zhou/coolweather/util/HttpUtil.java
@@ -10,7 +10,7 @@ public class HttpUtil {
     public static void sendOkHttpRequest(String address, Callback callback) {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder().url(address).build();
-        client.newCall(request).enqueue(callback);
+        client.newCall(request).enqueue(callback);//
 
     }
 }
